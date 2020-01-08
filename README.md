1d84751eaf655949bf879886b9b5ccbbd0188806
mameli <mameli93@gmail.com>
2020-01-08 20:23:21 +0100

    plots

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# test_batch_norm_paper
-Test the results of "How does batch normalization help optimization?" paper
+# Test batch norm paper
+In this repo, you can find two notebooks in which I tested the results of "How does batch normalization help optimization?" paper ([link](https://arxiv.org/abs/1805.11604))
+
+Batch normalization is a great technique to improve the performance of a deep neural network but despite the title of the original 2015 paper ([Batch Normalization: Accelerating Deep Network Training by Reducing Internal Covariate Shift](https://arxiv.org/abs/1502.03167)) the reduction of the Internal Covariate Shift seems to have little or nothing to do to the goodness of this method. 
+
+## Test setup
+- Pytorch VGG model config A with and without the batch norm layer after the 4th Conv layer
+- Xavier uniform initialization
+- Cross-Entropy loss
+- SGD optimizer
+- CIFAR 10 dataset
+- lr = 0.01
+
+## ICS
+In my testings, the internal covariate shift in the batch norm model was almost always higher compared to the model without batch norm...
+![alt](ICS.png)
+but the performance boost was obvious 
+![alt](ACC.png)
+
+## Why does Batch Norm work?
+The real reasons because batch normalization works are due to the higher gradient predictiveness and the more stable loss results. Being more confident with the direction of the gradients and having improved losses gives you the opportunity of increasing the learning rates and a faster and less sensitive to hyperparameter choices training.
+
+## Gradient predictiveness 
+The gradient predictiveness calculated as the norm 2 of (old_grad + new_grads) is clearly higher in the first stage of the training in the batch norm model
+![alt](grad.png)
+## Losses
+The loss landscape of the batch norm model is a little smoother
+![alt](loss.png)
--- batch_norm_callbacks.py
+++ batch_norm_callbacks.py
@@ -1,6 +1,8 @@
 from fastai import *
 from fastai.vision import *
-from batch_norm_vgg import VGG, make_layers
+from fastai.torch_core import add_metrics
+from fastai.callback import Callback
+from batch_norm_vgg import VGG, make_layers, make_layers_BN
 
 
 @dataclass
@@ -64,3 +66,69 @@ class AccuracyList(LearnerCallback):
         outs = last_output.argmax(dim=-1).view(n, -1)
         targs = last_target.view(n, -1)
         self.accs.append((outs == targs).float().mean())
+
+
+class AccuracyValidationList(Callback):
+    def __init__(self):
+        super().__init__()
+        self.name = "Val Acc"
+        self.val_accs = []
+
+    def on_epoch_begin(self, **kwargs):
+        self.accs = []
+        self.currentAcc = 0.
+
+    def on_batch_end(self, last_output, last_target, **kwargs):
+        n = last_target.shape[0]
+        outs = last_output.argmax(dim=-1).view(n, -1)
+        targs = last_target.view(n, -1)
+        self.accs.append((outs == targs).float().mean())
+
+    def on_epoch_end(self, last_metrics, **kwargs):
+        temp_val = sum(self.accs) / len(self.accs)
+        self.val_accs.append(temp_val)
+        return add_metrics(last_metrics, temp_val)
+
+
+@dataclass
+class LossVariation(LearnerCallback):
+    def __init__(self, learn: Learner):
+        super().__init__(learn)
+        self.loss_var = []
+        self.losses = []
+        self.old_loss = 0
+
+    def on_backward_begin(self, last_loss, **kwargs):
+        current_loss = last_loss.data.clone()
+        self.loss_var.append(
+            (self.old_loss - current_loss).norm(2).cpu().numpy())
+        self.losses.append(current_loss.cpu().numpy())
+        self.old_loss = current_loss
+
+
+@dataclass
+class GradVariation(LearnerCallback):
+    def __init__(self, learn: Learner):
+        super().__init__(learn)
+        self.grad_var = []
+        self.grads_l2 = []
+        self.old_grad = None
+        self.first_step = True
+
+        conv_layer_pos = 5
+        conv_counter = 0
+        self.grad_layer_pos = 0
+        for i, p in enumerate(self.learn.model.features):
+            if isinstance(p, nn.modules.Conv2d):
+                conv_counter += 1
+            if conv_counter == conv_layer_pos:
+                self.grad_layer_pos = i
+                break
+
+    def on_backward_end(self, **kwargs):
+        wg = self.learn.model.features[self.grad_layer_pos].weight.grad
+        if not self.first_step:
+            self.grad_var.append((self.old_grad + wg).abs().mean())
+            self.grads_l2.append((self.old_grad + wg).norm(2))
+        self.old_grad = wg.data.clone()
+        self.first_step = False
